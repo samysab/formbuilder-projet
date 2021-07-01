3992d22f73f8540cff3652dc6a5a10357d269bf9
samy sab <samy.saberi@gmail.com>
2021-07-01 10:34:40 +0200

    ajout du widget

--- includes/plg-functions.php
+++ includes/plg-functions.php
@@ -40,8 +40,64 @@ function plgesgi_TitiShortcode($atts){
 }
 
 
-add_action('widget_init', 'plgesgi_totoWidgets');
+// Widget
 
-function plgesgi_totoWidgets(){
-    register_widget('Toto_Widget');
+class PluginFormBuilder_widget extends WP_Widget{
+
+    public function __construct(){
+
+        parent::__constuct(
+            'pluginFormBuilder_widget',
+            __('Widget Form Builder', 'mypluginlg'),
+            array(
+                'description' => __('widget simple issu du plugin du form builder', 'monpluginlg')
+            )
+        );
+        add_action('wp_loaded', array($this, 'save_email'));
+    }
+
+    public function widget($args){
+        //Front
+        extract($args);
+        $title = apply_filters('widget_title', $instance['title']);
+        echo $before_widget.$before_title.$title.$after_title.'<form action="" method="post">
+                <label for="email_user"> Votre email :</label>
+                <input id="email_user" name="email_user" type="email">
+                <input type="submit">
+              </form>'.
+              $after_widget;
+    }
+
+    public function form($instance){
+        // Back
+        $title = isset($instance['title']) ? $instance['title'] : '';
+
+        echo '<p><label for="'.$this->get_field_name('title').'"> Titre : </label>
+        <input id="'.$this->get_field_id('title').'" name="'.$this->get_field_name('title').'" type="text" value="'.$title.'"></p>
+        ';
+    }
+
+    public function update($new_instance, $old_instance){
+        $instance = array();
+        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        return $instance;
+    }
+
+    public function save_email(){
+        if (isset($_POST['email_user']) && !empty($_POST['email_user'])){
+            global $wpdb;
+            $email = $_POST['email_user'];
+
+            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}toto_table WHERE email ='$email'");
+            if (is_null($row)){
+                $wpdb->insert("{$wpdb->prefix}toto_table", array('email' => $email));
+            }
+        }
+    }
+}
+
+add_action('widget_init', 'plgesgi_formWidgets');
+
+function plgesgi_formWidgets(){
+    register_widget('PluginFormBuilder_widget');
 }
