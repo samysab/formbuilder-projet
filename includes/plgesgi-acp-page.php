764360f344f9bfcf5902ee93496248510bbf54ac
Théo Sigaud <sigaudtheo@gmail.com>
2021-07-01 11:43:37 +0200

    Add script

--- includes/plgesgi-acp-page.php
+++ includes/plgesgi-acp-page.php
@@ -2,5 +2,11 @@
 
 defined('ABSPATH') or die('Accès refusé');
 
+wp_enqueue_script('script', plugin_dir_url(__FILE__) . 'script.js');
+
 
 echo "<h1> Bienvenue sur le plugin de contact </h1>";
+
+
+echo "<button>Créer un formulaire</button>";
+
