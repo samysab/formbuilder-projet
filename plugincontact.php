069be110c3139c0274a6e4dec8d744170ebd6b6f
suvithayah <suvithayah@gmail.com>
2021-07-29 12:07:26 +0200

    Fix enregistrement form

--- plugincontact.php
+++ plugincontact.php
@@ -17,7 +17,7 @@ register_activation_hook(__FILE__, 'plgesgi_activation');
 function plgesgi_activation(){
     global $wpdb;
     $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}contact_plugin (id INT AUTO_INCREMENT PRIMARY KEY , name VARCHAR(255) NOT NULL, type VARCHAR(255), status BOOLEAN);");
-    $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}contact_data (id INT AUTO_INCREMENT PRIMARY KEY , field_1 VARCHAR(255), field_2 VARCHAR(255),field_3 VARCHAR(255),field_4 VARCHAR(255);");
+    $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}contact_data (id INT AUTO_INCREMENT PRIMARY KEY , field_1 VARCHAR(255), field_2 VARCHAR(255),field_3 VARCHAR(255),field_4 VARCHAR(255));");
 }
 
 register_deactivation_hook( __FILE__, 'plgesgi_deactivation' );
